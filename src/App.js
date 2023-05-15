ea9f28605cd4be594b881398b160926a00c29139
gabriel barbosa <gabrielb_lima@hotmail.com>
2023-05-15 15:19:54 -0300

    teste final - com ajustes - letras

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Jogo from './Jogo';
 import Letras from './Letras';
 import palavras from './palavras';
@@ -14,108 +14,104 @@ let palavra = [];
 let underline = "";
 let restart = false;
 
-
 export default function App() {
+  const [estadoPalavra, setStatePalavra] = useState("");
+  const [erro, setErro] = useState(0);
+  const [iniciar, setIniciar] = useState(true);
+  const [statusJogo, setStatusJogo] = useState("");
+  const [imagem, setImagem] = useState(forca0);
+  const [letrasSelecionadas, setLetrasSelecionadas] = useState([]);
 
-const [estadoPalavra, setStatePalavra] = useState("");
-const [erro, setErro] = useState(0);
-const [iniciar, setIniciar] = useState(true);
-const [statusJogo, setStatusJogo] = useState("");
-const [imagem, setImagem] = useState(forca0);
+  const shuffle = () => Math.random() - 0.5;
 
-const shuffle = () => Math.random() - 0.5;
+  function restartPagina() {
+    if (restart) {
+      restart = true;
+      setStatePalavra('');
+      setErro(0);
+      setStatusJogo('');
+      setImagem(forca0);
+      setLetrasSelecionadas([]);
+    }
 
-function restartPagina(){
-  if (restart) {
-    restart = true;
-    setStatePalavra('');
-    setErro(0);
-    setStatusJogo('');
-    setImagem(forca0);
+    iniciarJogo();
   }
 
-  iniciarJogo();
-
-}
-
-function iniciarJogo() {
-  palavras.sort(shuffle);
-  palavra = Array.from(palavras[palavras.length - 1]);
-  setIniciar(false);
-  underline = "_".repeat(palavra.length);
-  setStatePalavra(underline);
-}
-
-function letraClicada(botoes) {
-  const positions = palavra.flatMap((letra, index) => letra === botoes ? index : []);
-  if (positions.length !== 0) {
-    const mapping = Array.from(underline);
-    positions.forEach((position) => {
-      mapping[position] = botoes;
-    });
-    underline = mapping.join("");
+  function iniciarJogo() {
+    palavras.sort(shuffle);
+    palavra = Array.from(palavras[palavras.length - 1]);
+    setIniciar(false);
+    underline = "_".repeat(palavra.length);
     setStatePalavra(underline);
-  } else {
-    setErro(erro + 1);
-    estadoForca();
   }
-  endGame();
-}
 
-function estadoForca(){
-  const imagensForca = [
-  forca0, 
-  forca1,
-  forca2,
-  forca3,
-  forca4,
-  forca5,
-  forca6,
-];
-
-setImagem(imagensForca[ erro + 1 ]);
-endGame();
-
-}
-
-function endGame(){
-  if (erro > 5 && underline !== palavra.join("")) {
-
-    setStatusJogo("Perdeu");
-    setIniciar(true);
-    setStatePalavra(palavra);
-    setErro(6);
-    setImagem(forca6); 
-    restart = true;
-  } else if (underline === palavra.join("")){
-    setStatusJogo("Ganhou"); 
-    setIniciar(true); 
-    restart = true;
+  function letraClicada(letra) {
+    const positions = palavra.flatMap((letraPalavra, index) =>
+      letraPalavra === letra ? index : []
+    );
+    if (positions.length !== 0) {
+      const mapping = Array.from(underline);
+      positions.forEach((position) => {
+        mapping[position] = letra;
+      });
+      underline = mapping.join("");
+      setStatePalavra(underline);
+    } else {
+      setErro(erro + 1);
+      estadoForca();
+    }
+    setLetrasSelecionadas([...letrasSelecionadas, letra]);
+    endGame();
   }
-}
 
-return (
-  <>
-    <Jogo
-      jogoIniciado={restartPagina}
-      estadoJogo={estadoPalavra}
-      iniciando={!iniciar}
-      novaPalavra={palavra.join("")}
-      statusJogo={statusJogo}
-      errorQtd={erro}
-      imagensForca={imagem}
-    />
-    <Letras 
-    iniciando={iniciar} letraEscolhida={letraClicada}
-    />
+  function estadoForca() {
+    const imagensForca = [
+      forca0,
+      forca1,
+      forca2,
+      forca3,
+      forca4,
+      forca5,
+      forca6,
+    ];
+
+    setImagem(imagensForca[erro + 1]);
+    endGame();
+  }
+
+  function endGame() {
+    if (erro > 5 && underline !== palavra.join("")) {
+      setStatusJogo("Perdeu");
+      setIniciar(true);
+      setStatePalavra(palavra);
+      setErro(6);
+      setImagem(forca6);
+      restart = true;
+    } else if (underline === palavra.join("")) {
+      setStatusJogo("Ganhou");
+      setIniciar(true);
+      restart = true;
+    }
+  }
+
+  return (
+    <>
+      <Jogo
+        jogoIniciado={restartPagina}
+        estadoJogo={estadoPalavra}
+        iniciando={!iniciar}
+        novaPalavra={palavra.join("")}
+        statusJogo={statusJogo}
+        errorQtd={erro}
+        imagensForca={imagem}
+      />
+      <Letras
+        iniciando={iniciar}
+        letraEscolhida={letraClicada}
+        letrasSelecionadas={letrasSelecionadas}
+      />
     </>
+  );
+}
 
-);
-};
-
-
-
-
-
-
-
+       
--- src/Letras.js
+++ src/Letras.js
@@ -1,49 +1,34 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 
-export default function Letras({ letraEscolhida, iniciando }) {
+export default function Letras({ letraEscolhida, iniciando, letrasSelecionadas }) {
   const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-  // Referência para controlar se os botões já foram reinicializados
-  const reiniciarRef = useRef(false);
-
-  // Função para reinicializar o estado dos botões
-  const reiniciarBotoes = () => {
-    reiniciarRef.current = true;
-  };
+  const isDisabled = (letra) => letrasSelecionadas.includes(letra);
 
   return (
     <div className="container-letras">
       <div className="letras">
         {alfabeto.map((letra) => (
-          <Botoes data-test="letter" key={letra} escolhido={() => letraEscolhida(letra)} iniciado={iniciando} reiniciar={reiniciarBotoes} botoes={letra.toUpperCase()} />
+          <Botoes
+            key={letra}
+            escolhido={() => letraEscolhida(letra)}
+            iniciado={iniciando}
+            disabled={isDisabled(letra)}
+            botoes={letra.toUpperCase()}
+          />
         ))}
       </div>
     </div>
   );
 }
 
-function Botoes({ escolhido, iniciado, reiniciar, botoes }) {
-  const [disabledBotao, setDisabledBotao] = useState(false);
-
-  function disable() {
-    setDisabledBotao(true);
-  }
-
-  // Verificar se os botões precisam ser reinicializados
-  if (reiniciar.current) {
-    setDisabledBotao(false);
-    reiniciar.current = false;
-  }
-
+function Botoes({ escolhido, iniciado, disabled, botoes }) {
   return (
     <button
       data-test="letter"
-      onClick={() => {
-        escolhido();
-        disable();
-      }}
-      className={iniciado || disabledBotao ? "letras-selecionadas" : "letras-nao-selecionadas"}
-      disabled={iniciado || disabledBotao}
+      onClick={escolhido}
+      className={iniciado || disabled ? "letras-selecionadas" : "letras-nao-selecionadas"}
+      disabled={iniciado || disabled}
     >
       {botoes}
     </button>
